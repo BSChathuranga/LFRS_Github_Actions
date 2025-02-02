2284a14a351000e7ee416635dd9fc190f28f80b7
VenuraDenethpriya <157047923+VenuraDenethpriya@users.noreply.github.com>
2025-02-02 14:49:16 +0530

    Update baseurl in frontend

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const reportApi = createApi({
   reducerPath: "reportApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "${import.meta.env.VITE_BACKEND_URL}/api/" }),
   /*prepareHeaders: async (headers, { getState}) => {
     const token = await window.__clerk_domain.session.getToken();
  
